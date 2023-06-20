4fcef26b9c4c422b57bf0906e121d47a7e91414b
ErickSalasT <l22390446@chetumal.tecnm.mx}>
2023-06-20 08:34:24 -0500

    repositorio

--- src/Compra.java
+++ src/Compra.java
@@ -1,62 +1,93 @@
 public class Compra {
 
-        private double precioEntrada;
-        private double precioComidaBuffet;
-        private double precioEspectaculo;
-        private double precioNadoDelfines;
+    private double precioEntrada;
+    private double precioComidaBuffet;
+    private double precioEspectaculo;
+    private double precioNadoDelfines;
+    private double precioVIP;
+    private double precioBalcon;
 
-        public Compra(double precioEntrada, double precioComidaBuffet, double precioEspectaculo, double precioNadoDelfines) {
-            this.precioEntrada = precioEntrada;
-            this.precioComidaBuffet = precioComidaBuffet;
-            this.precioEspectaculo = precioEspectaculo;
-            this.precioNadoDelfines = precioNadoDelfines;
-        }
+    public Compra(double precioEntrada, double precioComidaBuffet, double precioEspectaculo, double precioVIP, double precioBalcon, double precioNadoDelfines) {
+        this.precioEntrada = precioEntrada;
+        this.precioComidaBuffet = precioComidaBuffet;
+        this.precioEspectaculo = precioEspectaculo;
+        this.precioNadoDelfines = precioNadoDelfines;
+        this.precioVIP = precioVIP;
+        this.precioBalcon = precioBalcon;
+    }
 
-        public double getPrecioEntrada() {
-            return precioEntrada;
-        }
+    public double getPrecioEntrada() {
+        return precioEntrada;
+    }
 
-        public void setPrecioEntrada(double precioEntrada) {
-            this.precioEntrada = precioEntrada;
-        }
+    public void setPrecioEntrada(double precioEntrada) {
+        this.precioEntrada = precioEntrada;
+    }
 
-        public double getPrecioComidaBuffet() {
-            return precioComidaBuffet;
-        }
+    public double getPrecioComidaBuffet() {
+        return precioComidaBuffet;
+    }
 
-        public void setPrecioComidaBuffet(double precioComidaBuffet) {
-            this.precioComidaBuffet = precioComidaBuffet;
-        }
+    public void setPrecioComidaBuffet(double precioComidaBuffet) {
+        this.precioComidaBuffet = precioComidaBuffet;
+    }
 
-        public double getPrecioEspectaculo() {
+    public double getPrecioEspectaculo() {
+        return precioEspectaculo;
+    }
+
+    public void setPrecioEspectaculo(double precioEspectaculo) {
+        this.precioEspectaculo = precioEspectaculo;
+    }
+
+    public double getPrecioVIP() {
+        return precioVIP;
+    }
+
+    public void setPrecioVIP(double precioVIP) {
+        this.precioVIP = precioVIP;
+    }
+
+    public double getPrecioBalcon() {
+        return precioBalcon;
+    }
+
+    public void setPrecioBalcon(double precioBalcon) {
+        this.precioBalcon = precioBalcon;
+    }
+    public double getPrecioNadoDelfines() {
+        return precioNadoDelfines;
+    }
+
+    public void setPrecioNadoDelfines(double precioNadoDelfines) {
+        this.precioNadoDelfines = precioNadoDelfines;
+    }
+
+
+    public double calcularTotal(double precioEntrada) {
+        return precioEntrada;
+    }
+
+    public double calcularTotal(double precioEntrada, double precioComidaBuffet) {
+        return precioEntrada + precioComidaBuffet;
+    }
+
+    public double calcularTotal(double precioEntrada, double precioComidaBuffet, double precioEspectaculo) {
+        return precioEntrada + precioComidaBuffet + precioEspectaculo;
+    }
+
+    public double calcularTotal(double precioEntrada, double precioComidaBuffet, double precioEspectaculo, double precioNadoDelfines) {
+        return precioEntrada + precioComidaBuffet + precioEspectaculo + precioNadoDelfines;
+    }
+
+    public double getPrecioEspectaculo(String seccionLugar) {
+        if (seccionLugar.equalsIgnoreCase("VIP")) {
+            return precioVIP;
+        } else if (seccionLugar.equalsIgnoreCase("Balcón")) {
+            return precioBalcon;
+        } else {
             return precioEspectaculo;
         }
-
-        public void setPrecioEspectaculo(double precioEspectaculo) {
-            this.precioEspectaculo = precioEspectaculo;
-        }
-
-        public double getPrecioNadoDelfines() {
-            return precioNadoDelfines;
-        }
-
-        public void setPrecioNadoDelfines(double precioNadoDelfines) {
-            this.precioNadoDelfines = precioNadoDelfines;
-        }
-
-        public double calcularTotal(double precioEntrada) {
-            return precioEntrada;
-        }
-
-        public double calcularTotal(double precioEntrada, double precioComidaBuffet) {
-            return precioEntrada + precioComidaBuffet;
-        }
-
-        public double calcularTotal(double precioEntrada, double precioComidaBuffet, double precioEspectaculo) {
-            return precioEntrada + precioComidaBuffet + precioEspectaculo;
-        }
-
-        public double calcularTotal(double precioEntrada, double precioComidaBuffet, double precioEspectaculo, double precioNadoDelfines) {
-            return precioEntrada + precioComidaBuffet + precioEspectaculo + precioNadoDelfines;
-        }
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -12,14 +12,19 @@ public class Main {
         System.out.println("Ingrese el precio de la comida buffet:");
         double precioComidaBuffet = scanner.nextDouble();
 
-        System.out.println("Ingrese el precio del espectáculo nocturno:");
+        System.out.println("Ingrese el precio del espectáculo en general:");
         double precioEspectaculo = scanner.nextDouble();
+
+        System.out.println("Ingrese el precio por estar en la sección VIP:");
+        double precioVIP = scanner.nextDouble();
+
+        System.out.println("Ingrese el precio por estar en la sección Balcón:");
+        double precioBalcon = scanner.nextDouble();
 
         System.out.println("Ingrese el precio del nado con delfines:");
         double precioNadoDelfines = scanner.nextDouble();
 
-
-        Compra compra = new Compra(precioEntrada, precioComidaBuffet, precioEspectaculo, precioNadoDelfines);
+        Compra compra = new Compra(precioEntrada, precioComidaBuffet, precioEspectaculo, precioVIP, precioBalcon, precioNadoDelfines);
 
         System.out.println("Seleccione el paquete que desea comprar:");
         System.out.println("1. Entrada");
@@ -30,86 +35,101 @@ public class Main {
         int opcion = scanner.nextInt();
         double total = 0;
 
-    switch (opcion) {
-        case 1:
-            total = compra.calcularTotal(compra.getPrecioEntrada());
-            System.out.println("=======Detalle de la compra=======");
-            System.out.println("***Elementos adquiridos:***");
-            System.out.println(" - Entrada");
-            System.out.println("***Costos del paquete***");
-            System.out.println("Precio: $" + compra.getPrecioEntrada());
-            System.out.println("Total a pagar: $" + total);
-            break;
-        case 2:
-            total = compra.calcularTotal(compra.getPrecioEntrada(), compra.getPrecioComidaBuffet());
-            scanner.nextLine(); //Esto se puso para limpiar el buffer del scanner y así poder guardar los datos almacenados
-            System.out.println("Ingrese el nombre del platillo de la comida buffet:");
-            String nombrePlatillo = scanner.nextLine();
+        switch (opcion) {
+            case 1:
+                total = compra.calcularTotal(compra.getPrecioEntrada());
+                System.out.println("=======Detalle de la compra=======");
+                System.out.println("**Elementos adquiridos:**");
+                System.out.println(" - Entrada");
+                System.out.println("**Costos del paquete**");
+                System.out.println("Precio: $" + compra.getPrecioEntrada());
+                System.out.println("Total a pagar: $" + total);
+                break;
+            case 2:
+                total = compra.calcularTotal(compra.getPrecioEntrada(), compra.getPrecioComidaBuffet());
+                scanner.nextLine();
+                System.out.println("Ingrese el tipo de comida (Pastas, Caldos, Carnes, Mariscos):");
+                String tipoComida = scanner.nextLine();
+                while (!tipoComida.equalsIgnoreCase("Pastas") && !tipoComida.equalsIgnoreCase("Caldos") && !tipoComida.equalsIgnoreCase("Carnes") && !tipoComida.equalsIgnoreCase("Mariscos")) {
+                    System.out.println("Tipo de comida inválido. Ingrese el tipo de comida (Pastas, Caldos, Carnes, Mariscos):");
+                    tipoComida = scanner.nextLine();
+                }
+
+                System.out.println("=======Detalle de la compra=======");
+                System.out.println("**Elementos adquiridos:**");
+                System.out.println(" - Entrada");
+                System.out.println(" - Comida buffet: " + tipoComida);
+                System.out.println("**Costos del paquete**");
+                System.out.println("Precio entrada: $" + compra.getPrecioEntrada());
+                System.out.println("Precio comida buffet: $" + compra.getPrecioComidaBuffet());
+                System.out.println("Total a pagar: $" + total);
+                break;
+            case 3:
+                total = compra.calcularTotal(compra.getPrecioEntrada(), compra.getPrecioComidaBuffet(), compra.getPrecioEspectaculo());
+                scanner.nextLine();
+                System.out.println("Ingrese el tipo de comida (Pastas, Caldos, Carnes, Mariscos):");
+                String tipoComida2 = scanner.nextLine();
+                while (!tipoComida2.equalsIgnoreCase("Pastas") && !tipoComida2.equalsIgnoreCase("Caldos") && !tipoComida2.equalsIgnoreCase("Carnes") && !tipoComida2.equalsIgnoreCase("Mariscos")) {
+                    System.out.println("Tipo de comida inválido. Ingrese el tipo de comida (Pastas, Caldos, Carnes, Mariscos):");
+                    tipoComida = scanner.nextLine();
+                }
 
 
+                System.out.println("Ingrese la sección del lugar (Balcón, VIP, General) del espectáculo nocturno:");
+                String seccionLugar = scanner.nextLine();
+                while (!seccionLugar.equalsIgnoreCase("Balcón") && !seccionLugar.equalsIgnoreCase("VIP") && !seccionLugar.equalsIgnoreCase("General")) {
+                    System.out.println("Sección inválida. Ingrese la sección del lugar (Balcón, VIP, General) del espectáculo nocturno:");
+                    seccionLugar = scanner.nextLine();
+                }
 
-            System.out.println("=======Detalle de la compra=======");
-            System.out.println("***Elementos adquiridos:***");
-            System.out.println(" - Entrada");
-            System.out.println(" - Comida buffet: " + nombrePlatillo);
-            System.out.println("***Costos del paquete***");
-            System.out.println("Precio entrada: $" + compra.getPrecioEntrada());
-            System.out.println("Precio comida buffet: $" + compra.getPrecioComidaBuffet());
-            System.out.println("Total a pagar: $" + total);
-            break;
-        case 3:
-            total = compra.calcularTotal(compra.getPrecioEntrada(), compra.getPrecioComidaBuffet(), compra.getPrecioEspectaculo());
-            scanner.nextLine();
-            System.out.println("Ingrese el nombre del platillo de la comida buffet:");
-            String nombrePlatillo2 = scanner.nextLine();
+                System.out.println("=======Detalle de la compra=======");
+                System.out.println("**Elementos adquiridos:**");
+                System.out.println(" - Entrada");
+                System.out.println(" - Comida buffet: " + tipoComida2);
+                System.out.println(" - Espectáculo nocturno: " + seccionLugar);
+                System.out.println("**Costos del paquete**");
+                System.out.println("Precio entrada: $" + compra.getPrecioEntrada());
+                System.out.println("Precio comida buffet: $" + compra.getPrecioComidaBuffet());
+                System.out.println("Precio espectáculo nocturno: $" + compra.getPrecioEspectaculo(seccionLugar));
+                System.out.println("Total a pagar: $" + total);
+                break;
+            case 4:
+                total = compra.calcularTotal(compra.getPrecioEntrada(), compra.getPrecioComidaBuffet(), compra.getPrecioEspectaculo(), compra.getPrecioNadoDelfines());
+                scanner.nextLine();
+                System.out.println("Ingrese el tipo de comida (Pastas, Caldos, Carnes, Mariscos):");
+                String tipoComida3 = scanner.nextLine();
+                while (!tipoComida3.equalsIgnoreCase("Pastas") && !tipoComida3.equalsIgnoreCase("Caldos") && !tipoComida3.equalsIgnoreCase("Carnes") && !tipoComida3.equalsIgnoreCase("Mariscos")) {
+                    System.out.println("Tipo de comida inválido. Ingrese el tipo de comida (Pastas, Caldos, Carnes, Mariscos):");
+                    tipoComida = scanner.nextLine();
+                }
 
-            System.out.println("Ingrese la sección del lugar (Balcón, VIP, General) del espectáculo nocturno:");
-            String seccionLugar = scanner.nextLine();
+                System.out.println("Ingrese la sección del lugar (Balcón, VIP, General) del espectáculo nocturno:");
+                String seccionLugar2 = scanner.nextLine();
+                while (!seccionLugar2.equalsIgnoreCase("Balcón") && !seccionLugar2.equalsIgnoreCase("VIP") && !seccionLugar2.equalsIgnoreCase("General")) {
+                    System.out.println("Sección inválida. Ingrese la sección del lugar (Balcón, VIP, General) del espectáculo nocturno:");
+                    seccionLugar2 = scanner.nextLine();
+                }
 
-            System.out.println("=======Detalle de la compra=======");
-            System.out.println("***Elementos adquiridos:***");
-            System.out.println(" - Entrada");
-            System.out.println(" - Comida buffet: " + nombrePlatillo2);
-            System.out.println(" - Espectáculo nocturno: " + seccionLugar);
-            System.out.println("***Costos del paquete***");
-            System.out.println("Precio entrada: $" + compra.getPrecioEntrada());
-            System.out.println("Precio comida buffet: $" + compra.getPrecioComidaBuffet());
-            System.out.println("Precio espectáculo nocturno: $" + compra.getPrecioEspectaculo());
-            System.out.println("Total a pagar: $" + total);
-            break;
-        case 4:
-            total = compra.calcularTotal(compra.getPrecioEntrada(), compra.getPrecioComidaBuffet(), compra.getPrecioEspectaculo(), compra.getPrecioNadoDelfines());
-            scanner.nextLine();
-            System.out.println("Ingrese el nombre del platillo de la comida buffet:");
-            String nombrePlatillo3 = scanner.nextLine();
+                System.out.println("Ingrese el tipo de interacción (sencilla o extra) del nado con delfines:");
+                String tipoInteraccion = scanner.nextLine();
 
-            scanner.nextLine();
-
-            System.out.println("Ingrese la sección del lugar (Balcón, VIP, General) del espectáculo nocturno:");
-            String seccionLugar2 = scanner.nextLine();
-
-            scanner.nextLine();
-
-            System.out.println("Ingrese el tipo de interacción (sencilla o extra) del nado con delfines:");
-            String tipoInteraccion = scanner.nextLine();
-
-            System.out.println("=======Detalle de la compra=======");
-            System.out.println("***Elementos adquiridos:***");
-            System.out.println(" - Entrada");
-            System.out.println(" - Comida buffet: " + nombrePlatillo3);
-            System.out.println(" - Espectáculo nocturno: " + seccionLugar2);
-            System.out.println(" - Nado con delfines: " + tipoInteraccion);
-            System.out.println("***Costos del paquete***");
-            System.out.println("Precio entrada: $" + compra.getPrecioEntrada());
-            System.out.println("Precio comida buffet: $" + compra.getPrecioComidaBuffet());
-            System.out.println("Precio espectáculo nocturno: $" + compra.getPrecioEspectaculo());
-            System.out.println("Precio nado con delfines: $" + compra.getPrecioNadoDelfines());
-            System.out.println("Total a pagar: $" + total);
-            break;
-        default:
-            System.out.println("Opción inválida, captura una opcion valida");
-            break;
-    }
+                System.out.println("=======Detalle de la compra=======");
+                System.out.println("**Elementos adquiridos:**");
+                System.out.println(" - Entrada");
+                System.out.println(" - Comida buffet: " + tipoComida3);
+                System.out.println(" - Espectáculo nocturno: " + seccionLugar2);
+                System.out.println(" - Nado con delfines: " + tipoInteraccion);
+                System.out.println("**Costos del paquete**");
+                System.out.println("Precio entrada: $" + compra.getPrecioEntrada());
+                System.out.println("Precio comida buffet: $" + compra.getPrecioComidaBuffet());
+                System.out.println("Precio espectáculo nocturno: $" + compra.getPrecioEspectaculo());
+                System.out.println("Precio nado con delfines: $" + compra.getPrecioNadoDelfines());
+                System.out.println("Total a pagar: $" + total);
+                break;
+            default:
+                System.out.println("Opción inválida, captura una opción válida");
+                break;
+        }
 
     }
 }
